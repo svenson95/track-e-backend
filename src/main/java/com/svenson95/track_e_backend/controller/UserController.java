b4fb07fc7d0bb6675253af8f41317939ea5dceb7
Sven Brodny <svenbrodny@googlemail.com>
2025-08-22 00:42:47 +0200

    feat: getAllUsers endpoint "/"

--- src/main/java/com/svenson95/track_e_backend/controller/UserController.java
+++ src/main/java/com/svenson95/track_e_backend/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.svenson95.track_e_backend.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -19,6 +21,11 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @GetMapping("/")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 
     @PostMapping("/add")
     public User addUser(@RequestBody User user) {
