68ec17e8a7e849451787b7a24285f30be076c95d
Sven Brodny <svenbrodny@googlemail.com>
2025-08-22 11:13:51 +0200

    fix: cors error in ionic frontend (2)

--- src/main/java/com/svenson95/track_e_backend/config/CorsConfig.java
+++ src/main/java/com/svenson95/track_e_backend/config/CorsConfig.java
@@ -1,3 +1,5 @@
+package com.svenson95.track_e_backend.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
