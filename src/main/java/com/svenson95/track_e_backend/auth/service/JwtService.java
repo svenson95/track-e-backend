4520f264cc09bbb37bd86be5d5d6f156e368eb8a
Sven Brodny <svenbrodny@googlemail.com>
2025-08-25 21:13:24 +0200

    fix: deprecated jwt method

--- src/main/java/com/svenson95/track_e_backend/auth/service/JwtService.java
+++ src/main/java/com/svenson95/track_e_backend/auth/service/JwtService.java
@@ -1,5 +1,6 @@
 package com.svenson95.track_e_backend.auth.service;
 
+import java.security.Key;
 import java.util.Date;
 import java.util.Map;
 
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
@@ -15,12 +17,14 @@ public class JwtService {
     private static final long EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 7; // 7 days
 
     public String generateToken(Map<String, Object> userInfo) {
+        Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+
         return Jwts.builder()
                 .setSubject(userInfo.get("email").toString())
                 .setClaims(userInfo)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY.getBytes())
+                .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
 }
