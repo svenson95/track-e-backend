30128a18758f1d28a29d32767dee8b05f8efc936
Sven Brodny <svenbrodny@googlemail.com>
2025-08-25 21:48:33 +0200

    fix: jwt secret key (3)

--- src/main/java/com/svenson95/track_e_backend/auth/service/JwtService.java
+++ src/main/java/com/svenson95/track_e_backend/auth/service/JwtService.java
@@ -1,7 +1,5 @@
 package com.svenson95.track_e_backend.auth.service;
 
-import java.security.Key;
-import java.util.Base64;
 import java.util.Date;
 import java.util.Map;
 
@@ -9,7 +7,6 @@ import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
@@ -18,14 +15,12 @@ public class JwtService {
     private static final long EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 7; // 7 days
 
     public String generateToken(Map<String, Object> userInfo) {
-        Key key = Keys.hmacShaKeyFor(Base64.getDecoder().decode(SECRET_KEY));
-
         return Jwts.builder()
                 .setSubject(userInfo.get("email").toString())
                 .setClaims(userInfo)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(key, SignatureAlgorithm.HS256)
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY.getBytes())
                 .compact();
     }
 }
