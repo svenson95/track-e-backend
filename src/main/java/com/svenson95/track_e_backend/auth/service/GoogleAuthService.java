36ff5607e1816c2958ba21b8a1074d49c23b80be
Sven Brodny <svenbrodny@googlemail.com>
2025-09-02 16:04:15 +0200

    feat(auth): register user in mongodb after login

--- src/main/java/com/svenson95/track_e_backend/auth/service/GoogleAuthService.java
+++ src/main/java/com/svenson95/track_e_backend/auth/service/GoogleAuthService.java
@@ -5,17 +5,23 @@ import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
+import com.svenson95.track_e_backend.database.model.User;
+import com.svenson95.track_e_backend.database.repository.UserRepository;
 
 @Service
 public class GoogleAuthService {
 
     private static final String CLIENT_ID = "81384485805-o4b55e424moljjf98egavlhol819l18a.apps.googleusercontent.com";
+
+    @Autowired
+    private UserRepository userRepository;
 
     public Map<String, Object> verifyToken(String idTokenString) {
         try {
@@ -28,6 +34,7 @@ public class GoogleAuthService {
             GoogleIdToken idToken = verifier.verify(idTokenString);
             if (idToken != null) {
                 GoogleIdToken.Payload payload = idToken.getPayload();
+                this.registerUser(payload);
 
                 return Map.of(
                         "userId", payload.getSubject(),
@@ -40,6 +47,23 @@ public class GoogleAuthService {
             }
         } catch (IOException | GeneralSecurityException e) {
             return null;
+        }
+    }
+
+    private void registerUser(GoogleIdToken.Payload payload) {
+        String userId = payload.getSubject();
+
+        if (!userRepository.existsById(userId)) {
+            User user = new User();
+
+            user.setId(userId);
+            user.setEmail(payload.getEmail());
+            user.setName((String) payload.get("name"));
+            user.setPicture((String) payload.get("picture"));
+            user.setWeight((Number) payload.get("weight"));
+            user.setHeight((Number) payload.get("height"));
+
+            userRepository.save(user);
         }
     }
 }
--- src/main/java/com/svenson95/track_e_backend/database/model/User.java
+++ src/main/java/com/svenson95/track_e_backend/database/model/User.java
@@ -9,15 +9,19 @@ public class User {
     private String id;
 
     private String name;
+    private String picture;
     private String email;
     private Number weight;
+    private Number height;
 
     public User() {}
 
-    public User(String name, String email, Number weight) {
+    public User(String name, String picture, String email, Number weight, Number height) {
         this.name = name;
+        this.picture = picture;
         this.email = email;
         this.weight = weight;
+        this.height = height;
     }
 
     public String getId() { return id; }
@@ -26,9 +30,15 @@ public class User {
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
+    public String getPicture() { return picture; }
+    public void setPicture(String picture) { this.picture = picture; }
+
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
 
     public Number getWeight() { return weight; }
     public void setWeight(Number weight) { this.weight = weight; }
+
+    public Number getHeight() { return height; }
+    public void setHeight(Number height) { this.height = height; }
 }
