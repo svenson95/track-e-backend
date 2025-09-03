63edbad870623c1addd50bf500f730892bf7cdd3
Sven Brodny <svenbrodny@googlemail.com>
2025-09-03 11:49:40 +0200

    fix: get env vars (6) test

--- src/main/java/com/svenson95/track_e_backend/auth/service/GoogleAuthService.java
+++ src/main/java/com/svenson95/track_e_backend/auth/service/GoogleAuthService.java
@@ -6,7 +6,6 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
 import com.svenson95.track_e_backend.database.model.User;
 import com.svenson95.track_e_backend.database.repository.UserRepository;
-import jakarta.annotation.PostConstruct;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
@@ -19,11 +18,6 @@ public class GoogleAuthService {
   static String CLIENT_ID = System.getenv("GOOGLE_CLIENT_ID");
 
   @Autowired private UserRepository userRepository;
-
-  @PostConstruct
-  public void logGoogleClientId() {
-    System.out.println(">>> GOOGLE_CLIENT_ID = " + CLIENT_ID);
-  }
 
   public Map<String, Object> verifyToken(String idTokenString) {
     try {
--- src/main/java/com/svenson95/track_e_backend/config/CorsConfig.java
+++ src/main/java/com/svenson95/track_e_backend/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.svenson95.track_e_backend.config;
 
-import jakarta.annotation.PostConstruct;
 import java.util.Arrays;
 import java.util.Objects;
 import org.springframework.context.annotation.Bean;
@@ -11,17 +10,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
   static String DEV_ENV = "http://localhost:8100";
-  static String PROD_ENV = System.getenv("PROD_ENV");
+  static String PROD_ENV = "https://trackthat.vercel.app";
 
   String[] origins =
       Arrays.stream(new String[] {DEV_ENV, PROD_ENV})
           .filter(Objects::nonNull)
           .toArray(String[]::new);
-
-  @PostConstruct
-  public void logEnv() {
-    System.out.println(">>> PROD_ENV = " + PROD_ENV);
-  }
 
   @Bean
   public WebMvcConfigurer corsConfigurer() {
