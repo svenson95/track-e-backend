dd49b40472e4b0255087046a16ec6c3a51d9da1b
Sven Brodny <svenbrodny@googlemail.com>
2025-09-03 11:31:38 +0200

    fix: get env vars (4)

--- src/main/java/com/svenson95/track_e_backend/config/CorsConfig.java
+++ src/main/java/com/svenson95/track_e_backend/config/CorsConfig.java
@@ -1,5 +1,8 @@
 package com.svenson95.track_e_backend.config;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -12,6 +15,10 @@ public class CorsConfig {
     static String DEV_ENV = "http://localhost:8100";
     static String PROD_ENV = System.getenv("PROD_ENV");
 
+    String[] origins = Arrays.stream(new String[]{DEV_ENV, PROD_ENV})
+                    .filter(Objects::nonNull)
+                    .toArray(String[]::new);
+
     @PostConstruct
     public void logEnv() {
         System.out.println(">>> PROD_ENV = " + PROD_ENV);
@@ -23,9 +30,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(DEV_ENV, PROD_ENV)
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
+                    .allowedOrigins(origins)
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*");
             }
         };
     }
