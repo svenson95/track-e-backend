608627adf096322dc799f078923ef9bb3ee1588a
Sven Brodny <svenbrodny@googlemail.com>
2025-09-04 10:02:53 +0200

    refactor(auth): auto save user in google login (2)

--- src/main/java/com/svenson95/track_e_backend/auth/service/GoogleAuthService.java
+++ src/main/java/com/svenson95/track_e_backend/auth/service/GoogleAuthService.java
@@ -4,7 +4,6 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
-import com.svenson95.track_e_backend.database.model.User;
 import com.svenson95.track_e_backend.database.repository.UserRepository;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
@@ -30,7 +29,6 @@ public class GoogleAuthService {
       GoogleIdToken idToken = verifier.verify(idTokenString);
       if (idToken != null) {
         GoogleIdToken.Payload payload = idToken.getPayload();
-        this.registerUser(payload);
 
         return Map.of(
             "userId",
@@ -46,23 +44,6 @@ public class GoogleAuthService {
       }
     } catch (IOException | GeneralSecurityException e) {
       return null;
-    }
-  }
-
-  private void registerUser(GoogleIdToken.Payload payload) {
-    String userId = payload.getSubject();
-
-    if (!userRepository.existsByGoogleId(userId)) {
-      User user = new User();
-
-      user.setGoogleId(userId);
-      user.setEmail(payload.getEmail());
-      user.setName((String) payload.get("name"));
-      user.setPicture((String) payload.get("picture"));
-      user.setWeight(0);
-      user.setHeight(0);
-
-      userRepository.save(user);
     }
   }
 }
