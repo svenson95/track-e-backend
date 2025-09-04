0c605c8727a838d6d399fc739a5a1997e1035c3d
Sven Brodny <svenbrodny@googlemail.com>
2025-09-04 10:00:18 +0200

    feat(auth): auto save user in google login

--- src/main/java/com/svenson95/track_e_backend/auth/controller/AuthController.java
+++ src/main/java/com/svenson95/track_e_backend/auth/controller/AuthController.java
@@ -2,7 +2,10 @@ package com.svenson95.track_e_backend.auth.controller;
 
 import com.svenson95.track_e_backend.auth.service.GoogleAuthService;
 import com.svenson95.track_e_backend.auth.service.JwtService;
+import com.svenson95.track_e_backend.database.model.User;
+import com.svenson95.track_e_backend.database.repository.UserRepository;
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,25 +19,44 @@ public class AuthController {
   private final GoogleAuthService googleAuthService;
   private final JwtService jwtService;
 
+  @Autowired private UserRepository userRepository;
+
   public AuthController(GoogleAuthService googleAuthService, JwtService jwtService) {
     this.googleAuthService = googleAuthService;
     this.jwtService = jwtService;
   }
 
   @PostMapping("/google")
-  public ResponseEntity<?> loginWithGoogle(@RequestBody Map<String, String> body) {
+  public ResponseEntity<Map<String, Object>> loginWithGoogle(
+      @RequestBody Map<String, String> body) {
     String idToken = body.get("token");
 
     Map<String, Object> userInfo = googleAuthService.verifyToken(idToken);
     if (userInfo == null) {
-      return ResponseEntity.status(401).body("Invalid Google Token");
+      return ResponseEntity.status(401).body(Map.of("error", "Invalid Google Token"));
     }
 
     String jwt = jwtService.generateToken(userInfo);
 
+    User user =
+        userRepository
+            .findByGoogleId((String) userInfo.get("userId"))
+            .orElseGet(
+                () -> {
+                  User newUser = new User();
+                  newUser.setGoogleId((String) userInfo.get("userId"));
+                  newUser.setEmail((String) userInfo.get("email"));
+                  newUser.setName((String) userInfo.get("name"));
+                  newUser.setPicture((String) userInfo.get("picture"));
+                  newUser.setWeight(0);
+                  newUser.setHeight(0);
+                  userRepository.save(newUser);
+                  return newUser;
+                });
+
     return ResponseEntity.ok(
         Map.of(
             "token", jwt,
-            "user", userInfo));
+            "user", user));
   }
 }
